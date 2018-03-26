d35e9a6ac56727a377b38bd0b6246803c3707e8e
binginto <binginto@163.com>
2018-03-26 14:07:53 +0800

    feat: test

--- backend/task/init-all-blog.js
+++ backend/task/init-all-blog.js
@@ -4,9 +4,25 @@ const marked = require('marked')
 const path = require('path')
 process.env.mock = true
 
+const category = 'blog'
+const banner = Database.Random.image('100x100')
+const author = 'By Binginto'
+const introduction = '简单介绍2'
+
 describe('init', async () => {
   it('markdown', async () => {
     const blogs = await fs.readdirSync(path.resolve(__dirname, './blog'))
-    console.log(blogs)
+    for (let i = 0; i < blogs.length; i++) {
+      const blog = await fs.readFileSync(path.resolve(__dirname, './blog/' + blogs[i] ))
+      const htmlStr = marked(blog.toString())
+      await Database.Article.mock({
+        category: category,
+        title: blogs[i],
+        banner: banner,
+        author: author,
+        introduction: introduction,
+        content: htmlStr
+      })
+    }
   })
 })
