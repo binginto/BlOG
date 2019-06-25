d2edd4390d9f9fbc6997ba218e1a51ca9b41ea18
binginto <ouyangbc@gzmoninet.com>
2019-06-25 17:13:32 +0800

    feat: 更新

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ BLOG
 
 windows下 创建软连接
 - mklink /d  F:\Binginto\BlOG\backend\node_modules\app F:\Binginto\BlOG\backend
+
+
+书写文档
+1、全局安装
+cnpm install --global mocha
+2、运行文章
+mocha ./task/createBlog.js
--- backend/task/createBlog.js
+++ backend/task/createBlog.js
@@ -5,23 +5,23 @@ const path = require('path')
 process.env.mock = true
 
 const category = 'blog'
-const title = '常见的http状态码'
+const title = '测试'
 const banner = Database.Random.image('100x100')
 const author = 'By Binginto'
 const introduction = '简单介绍2'
 
-describe('init', async () => {
-  it('markdown', async () => {
-    const blog = await fs.readFileSync(path.resolve(__dirname, './blog/' + title + '.md'))
-    const htmlStr = marked(blog.toString())
-    console.log(htmlStr)
-    Database.Article.mock({
-      category: category,
-      title: title,
-      banner: banner,
-      author: author,
-      introduction: introduction,
-      content: htmlStr
+describe('init', async() => {
+    it('markdown', async() => {
+        const blog = await fs.readFileSync(path.resolve(__dirname, './blog/' + title + '.md'))
+        const htmlStr = marked(blog.toString())
+        console.log(htmlStr)
+        Database.Article.mock({
+            category: category,
+            title: title,
+            banner: banner,
+            author: author,
+            introduction: introduction,
+            content: htmlStr
+        })
     })
-  })
 })
